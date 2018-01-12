c1e477419d324f82e31d90c5e4cca23ce74f68cd
ghetolay <ghetolay@gmail.com>
2018-01-12 11:53:23 +0100

    update to angular v5

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,17 @@ export { CorrectorsValidator } from './correctors.validator';
 
 export * from './model';
 
-import { CorrectorFn } from './model';
-
 import { isNumber } from './correctors/isnumber';
 import { maxInt } from './correctors/maxint';
 import { maxLength } from './correctors/maxlength';
 import { minLength } from './correctors/minlength';
 
+//@ts-ignore need to import to avoid error on infer type but they complain it's unused
+//should not be needed anymore with 2.7
+import { CorrectorFn } from './model';
 export const Correctors = {
-	isNumber: isNumber,
-	maxInt: maxInt,
-	maxLength: maxLength,
-	minLength: minLength
+	isNumber,
+	maxInt,
+	maxLength,
+	minLength
 };
